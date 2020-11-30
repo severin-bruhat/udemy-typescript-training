2ba27e044d8b84b1ccdced1f281e0f53a218cf34
Severin Bruhat <sbruhat@gmail.com>
2020-11-30 21:40:29 +0000

    add an Interface to catch the errors

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,15 +2,21 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then(response => {
-    const todo = response.data;
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
 
-    const ID = todo.ID;
-    const title = todo.Title;
-    const finished = todo.finished;
+axios.get(url).then(response => {
+    const todo = response.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const finished = todo.completed;
 
     console.log(`
-        The Todo with ID/ ${ID}
+        The Todo with ID: ${id}
         Has a title of: ${title}
         Is it finished? ${finished}
     `)
