b59508d42327e3dce86a07442efc01730e30b706
Severin Bruhat <sbruhat@gmail.com>
2020-12-31 17:34:44 +0000

    end of classes tutorial

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,29 @@
 class Vehicle {
+
+    public constructor(public color: string) {}
+
     protected honk(): void {
         console.log('bip');
     }
 }
 
+const vehicule = new Vehicle('orange');
+console.log(vehicule.color);
+
 class Car extends Vehicle{
+    public constructor(public wheels: number, color: string) {
+        super(color);
+    }
+
     private drive(): void {
         console.log('vroom vromm');
     }
-
+ 
     public startDrivingProcess(): void {
         this.drive();
         this.honk();
     }
 }
 
-const car = new Car();
+const car = new Car(4, 'blue');
 car.startDrivingProcess();
