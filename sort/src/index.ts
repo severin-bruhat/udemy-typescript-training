a05b33c17495d65e228101449019d8c43d2e5198
Severin Bruhat <sbruhat@gmail.com>
2021-01-03 14:12:37 +0000

    tidy up

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -3,10 +3,11 @@ import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
 
 const numbersCollection = new NumbersCollection([53, 3332, 99, 0, -10, 88]);
-const charactersCollection = new CharactersCollection("Hello");
 const numberSorter = new Sorter(numbersCollection);
-const charSorter = new Sorter(charactersCollection);
 numberSorter.sort();
-charSorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection("Hello");
+const charSorter = new Sorter(charactersCollection);
+charSorter.sort();
 console.log(charactersCollection.data);
