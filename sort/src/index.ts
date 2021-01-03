5da0bcbed7eaf316355f97fba8a2eddf461839ef
Severin Bruhat <sbruhat@gmail.com>
2021-01-03 11:09:22 +0000

    init the bubble sort algorithm

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,3 +1,22 @@
-console.log('Hi there');
+class Sorter {
+    constructor(public collection: number[]) {}
 
-console.log('123');
+    sort(): void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) { //-i to ignore the last item which is already the biggest one
+               if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+               }
+            }  
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
+
