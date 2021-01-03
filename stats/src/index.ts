5facdf49dd1928c81e7ff1354b24243aed146188
Severin Bruhat <sbruhat@gmail.com>
2021-01-03 17:21:22 +0000

    start to analyse some data - 'bad' implementation

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,22 @@
 import fs from 'fs';
+
+const matches = fs
+    .readFileSync('football.csv', {
+        encoding: 'utf-8'
+    })
+    .split('\n')
+    .map((row: string): string[] => {
+        return row.split(',');
+    });
+
+let manUnitedWind = 0;
+
+for(let match of matches) {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWind++;
+    } else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWind++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWind} games`);
