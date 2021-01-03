0731f37c56622628c3adddf79f32eba7f18edda9
Severin Bruhat <sbruhat@gmail.com>
2021-01-03 14:23:42 +0000

    add the LinkedList sorter logic

--- sort/src/LinkedList.ts
+++ sort/src/LinkedList.ts
@@ -1,23 +1,88 @@
-// import { Sortable } from './Sorter'
+class Node {
+    next: Node | null = null;
+  
+    constructor(public data: number) {}
+}
+  
+export class LinkedList {
+    head: Node | null = null;
 
-// export class LinkedList implements Sortable {
-//     data: number[];
-    
-//     constructor(data: number[]) {
-//         this.data = data;
-//     }
+    add(data: number): void {
+        const node = new Node(data);
 
-//     get length(): number {
-//         return this.data.length;
-//     }
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
 
-//     compare(leftIndex: number, rightIndex: number): boolean {
-//         return this.data[leftIndex] > this.data[rightIndex];
-//     };
+        let tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
 
-//     swap(leftIndex: number, rightIndex: number): void {
-//         const leftHand = this.data[leftIndex];
-//         this.data[leftIndex] = this.data[rightIndex];
-//         this.data[rightIndex] = leftHand;
-//     };
-// }
+        tail.next = node;
+    }
+
+    get length(): number {
+        if (!this.head) {
+            return 0;
+        }
+
+        let length = 1;
+        let node = this.head;
+        while (node.next) {
+            length++;
+            node = node.next;
+        }
+
+        return length;
+    }
+
+    at(index: number): Node {
+        if (!this.head) {
+            throw new Error('Index out of bounds');
+        }
+
+        let counter = 0;
+        let node: Node | null = this.head;
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+
+            counter++;
+            node = node.next;
+        }
+
+        throw new Error('Index out of bounds');
+    }
+
+    compare(leftIndex: number, rightIndex: number): boolean {
+        if (!this.head) {
+            throw new Error('List is empty');
+        }
+
+        return this.at(leftIndex).data > this.at(rightIndex).data;
+    }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        const leftNode = this.at(leftIndex);
+        const rightNode = this.at(rightIndex);
+
+        const leftHand = leftNode.data;
+        leftNode.data = rightNode.data;
+        rightNode.data = leftHand;
+    }
+
+    print(): void {
+        if (!this.head) {
+            return;
+        }
+
+        let node: Node | null = this.head;
+        while (node) {
+            console.log(node.data);
+            node = node.next;
+        }
+    }
+}
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,13 +1,29 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
 
 const numbersCollection = new NumbersCollection([53, 3332, 99, 0, -10, 88]);
 const numberSorter = new Sorter(numbersCollection);
 numberSorter.sort();
+console.log("numbersCollection");
 console.log(numbersCollection.data);
+console.log("\r");
 
 const charactersCollection = new CharactersCollection("Hello");
 const charSorter = new Sorter(charactersCollection);
 charSorter.sort();
+console.log("charactersCollection");
 console.log(charactersCollection.data);
+console.log("\r");
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+console.log("linkedList");
+linkedList.print();
