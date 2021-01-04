d096c3dae8152f8d623ea1d06bb94728c563c3ed
Severin Bruhat <sbruhat@gmail.com>
2021-01-04 17:54:22 +0000

    refactoring using Generics in order to make the code reusable

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,13 +1,11 @@
 import fs from 'fs';
-import { dateStringToDate } from './utils'
-import { MatchResult } from './MatchResult';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]; //Tuple
-
-export class CsvFileReader {
-    data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
 
     constructor(public filename: string) {}
+
+    abstract mapRow(row: string []): T;
 
     read(): void {
         this.data = fs
@@ -20,19 +18,7 @@ export class CsvFileReader {
             return row.split(',');
             }
         )
-        .map(
-            (row: string[]): MatchData => {
-                return [
-                    dateStringToDate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[3]),
-                    parseInt(row[4]),
-                    row[5] as MatchResult,
-                    row[6],
-                ];
-            }
-        );
+        .map(this.mapRow);
     }
 
 }
---
+++ stats/src/generics.ts
@@ -0,0 +1,12 @@
+// There are just examples to cover the usage of GENERIC
+
+// class HoldAnything<T> {
+//     data: T;
+// }
+
+// const holdNumber = new HoldAnything<number>();
+// holdNumber.data = 123;
+
+// const holdString =new HoldAnything<string>();
+// holdString.data = "Hello";
+
