03b6a91e0b8021832ec59d2b6a0e8e78a9a8d9f2
Severin Bruhat <sbruhat@gmail.com>
2021-01-09 16:56:49 +0000

    add the setter

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User'
 
 const user = new User({name: 'Severin', age: 35});
 
+user.set({ name: 'newName' });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
