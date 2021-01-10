22bdc78f70e63a0190f5ae11dd44a2a7d726e9d6
Severin Bruhat <sbruhat@gmail.com>
2021-01-10 18:42:06 +0000

    finish the event system - add the trigger

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,12 +3,18 @@ import { User } from './models/User'
 const user = new User({name: 'Severin', age: 35});
 
 user.on('change', () => {
-
+    console.log('Change #1');
 });
 
-user.on('click', () => {
+user.on('change', () => {
+    console.log('Change #2');
+});
 
+user.on('save', () => {
+    console.log('Save was triggered');
 });
 
 
+user.trigger('change');
+user.trigger('save');
 console.log(user);
