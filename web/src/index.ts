96263ac0d2074c2554ddd65ebe518e6006560f2f
Severin Bruhat <sbruhat@gmail.com>
2021-01-11 21:03:12 +0000

    add fetch method

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,5 @@
-import axios from 'axios';
+import { User } from './models/User';
 
-axios.post('http://localhost:3000/users', {
-    name: 'myname',
-    age: 20
-});
+const user = new User({ id: 2 });
 
-const user = axios.get('http://localhost:3000/users/1');
+user.fetch();
