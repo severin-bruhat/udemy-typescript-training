0a8a15d56bbf7852e8bbf706738167b852183d20
Severin Bruhat <sbruhat@gmail.com>
2021-01-14 20:49:16 +0000

    implement the fetch

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({  name: 'Bob', age: 34});
+const user = new User({  id: 1});
 
 user.on('change', () => {
-    console.log('user was changed');
+    console.log(user);
 });
 
-user.trigger('change');
+user.fetch();
