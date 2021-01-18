bc5b60372e1a5ae91c5d271e365e45ed2be11e94
Severin Bruhat <sbruhat@gmail.com>
2021-01-18 21:14:13 +0000

    implement the save method

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -6,8 +6,13 @@ export class UserForm extends View<User, UserProps> {
     eventsMap(): { [key: string]: () => void} {
         return {
             'click:.set-age': this.onSetAgeClick,
-            'click:.set-name': this.onSetNameClick
+            'click:.set-name': this.onSetNameClick,
+            'click:.save-model': this.onSaveClick
         };
+    }
+
+    onSaveClick = (): void => {
+        this.model.save();
     }
 
     onSetAgeClick = (): void => {
@@ -26,12 +31,10 @@ export class UserForm extends View<User, UserProps> {
     template(): string {
         return `
             <div>
-                <h1>User Form</h1>
-                <div>User name: ${this.model.get('name')}</div>
-                <div>User age: ${this.model.get('age')}</div>
-                <input />
+                <input placeholder="${this.model.get('name')}"/>
                 <button class="set-name">Change name</button>
                 <button class="set-age">Set random age</button>
+                <button class="save-model">Save User</button>
             </div>
         `;
     }
