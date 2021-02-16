072f7cf97a6f780fa23752e21c4cc47794782f8d
Severin Bruhat <sbruhat@gmail.com>
2021-02-16 17:30:29 +0000

    add a decorator factory

--- features/decorators.ts
+++ features/decorators.ts
@@ -5,21 +5,23 @@ class Boat {
         return `This boat's color is ${this.color}`;
     }
 
-    @logError
+    @logError('Oops, the boat sunk')
     pilot(): void {
         throw new Error();
         console.log('swish');
     }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-    const method = desc.value; //reference to pilot()
-
-    desc.value = function(){
-        try {
-            method();
-        } catch(e) {
-            console.log('Oops');
+function logError(errorMessage: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor): void {
+        const method = desc.value; //reference to pilot()
+    
+        desc.value = function(){
+            try {
+                method();
+            } catch(e) {
+                console.log(errorMessage);
+            }
         }
     }
 }
